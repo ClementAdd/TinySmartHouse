041513bd24a97a8865a11ae738a1d11c92e6aabb
luludulDev <dulin.lucas21@gmail.com>
2023-11-28 10:06:40 +0100

    fix

--- iot/src/app.module.ts
+++ iot/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SensorsServiceModule } from './sensors_service/sensors_service.module';
-import { SensorsServiceModule } from './sensors_service/sensors_service.module';
 
 @Module({
   imports: [SensorsServiceModule],
