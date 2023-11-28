65f8d9175b640e097cf2dc29182640b0f692a56d
luludulDev <dulin.lucas21@gmail.com>
2023-11-28 11:30:37 +0100

    sensor service and controller improved - to test

--- iot/src/sensors/sensors.controller.ts
+++ iot/src/sensors/sensors.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { SensorsService } from './sensors.service';
+import { SerialPort } from 'serialport';
 
 @Controller('sensors')
 export class SensorsController {
 
     constructor(private readonly sensorsService: SensorsService) {}
+
+    public static housePort = new SerialPort({
+        path: '/dev/tty.usbserial-1130',
+        baudRate: 9600,
+        autoOpen: true
+    })
 
     @Get()
     getTemperature(): string {
--- iot/src/sensors/sensors.service.ts
+++ iot/src/sensors/sensors.service.ts
@@ -1,20 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { SensorsController } from './sensors.controller';
 
 @Injectable()
 export class SensorsService {
 
     getTemperature(): string {
         const strTemperature = "";
+
+        const temp = SensorsController.housePort.write('GET_TEMP');
+        //return temp;
+
         return strTemperature;
     }
 
     getBarometry(): string {
         const strBarometry = "";
+
+        const baro = SensorsController.housePort.write('GET_BARO');
+
         return strBarometry;
     }
 
     getHygrometry(): string {
         const strHygrometry = "";
+
+        const hygro = SensorsController.housePort.write('GET_HUMIDITY');
+        const hygrotest = SensorsController.housePort.read();
+
+        console.log("hygro");
+        console.log(hygro);
+        console.log("hygrotest");
+        console.log(hygrotest);
+
         return strHygrometry;
     }
 }
